f525d5661a28e8ba57eda4b63c67b6419be70114
jeun0415 <j_eun0415@naver.com>
2024-12-30 12:11:34 +0900

    friendService 정리

--- friendService/src/main/java/com/example/friendservice/repository/CalendarRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.friendservice.repository;
-
-import com.example.friendservice.entity.Calendars;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CalendarRepository extends JpaRepository<Calendars, Long> {
-
-}
